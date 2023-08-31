8871040c41bf02ff5e3c0f8ad9df4ea8978d70e2
Danil Kuznetsov <danil.kuznetsov@exness.com>
2023-09-01 00:03:45 +0300

    Add postgres deployment

--- deployments.sh
+++ deployments.sh
@@ -3,3 +3,6 @@ docker run -d -p 3000:3000 --name=grafana grafana/grafana-enterprise
 
 # https://hub.docker.com/_/mongo
 docker run -d -p 27017:27017 --name mongo -e MONGO_INITDB_ROOT_USERNAME=mongoadm -e MONGO_INITDB_ROOT_PASSWORD=mongoadm mongo:4.4.24
+
+# https://hub.docker.com/_/postgres
+docker run -d -p 5432:5432 --name postgres -e POSTGRES_USER=postgresadm -e POSTGRES_PASSWORD=postgresadm postgres
